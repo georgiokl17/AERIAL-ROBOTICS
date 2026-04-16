103003e22b919ff6668545af870863320b000961
Georgio Nasr <georgionasr2014@gmail.com>
2026-04-16 17:57:51 +0200

    adding error plots

--- src/plot_image.py
+++ src/plot_image.py
@@ -64,44 +64,51 @@ ax[2,0].set_xlabel('time')
 ax[2,0].legend()
 ax[2,0].grid()
 
+mynhfclog = pd.read_csv(
+    '../logs/01_Quadrotor/nhfc.log',
+    delimiter=r'\s+',
+    engine='python',
+    na_values=['-', 'nan'],
+    comment='#'
+)
+time2 = mynhfclog['ts']-mylog['ts'].iloc[0]
+fig,ax = plt.subplots(2,2)
 
-# fig,ax = plt.subplots(2,2)
+#subplot of positions errors 
+ax[0,0].set_ylim(-2,2)
+ax[0,0].plot(time2, mynhfclog['e_x'], color='red', label='x_err')
+ax[0,0].plot(time2, mynhfclog['e_y'], color='green', label='y_err')
+ax[0,0].plot(time2, mynhfclog['e_z'], color='blue',label='z_err')
+ax[0,0].set_xlabel('time')
+ax[0,0].legend()
+ax[0,0].grid()
 
-# #subplot of x y z values
-# ax[0,0].set_ylim(-2,2)
-# ax[0,0].plot(time, mylog['x'], color='red', label='x_pos')
-# ax[0,0].plot(time, mylog['y'], color='green', label='y_pos')
-# ax[0,0].plot(time, mylog['z'], color='blue',label='z_pos')
-# ax[0,0].set_xlabel('time')
-# ax[0,0].legend()
-# ax[0,0].grid()
+#subplot of roll pitch yaw errors
+ax[0,1].set_ylim(-4,4)
+ax[0,1].plot(time2, mynhfclog['e_rx'], color='red', label='roll_err')
+ax[0,1].plot(time2, mynhfclog['e_ry'], color='green', label='pitch_err')
+ax[0,1].plot(time2, mynhfclog['e_rz'], color='blue',label='yaw_err')
+ax[0,1].set_xlabel('time')
+ax[0,1].legend()
+ax[0,1].grid()
 
-# #subplot of roll pitch yaw values
-# ax[0,1].set_ylim(-4,4)
-# ax[0,1].plot(time, mylog['roll'], color='red', label='roll')
-# ax[0,1].plot(time, mylog['pitch'], color='green', label='pitch')
-# ax[0,1].plot(time, mylog['yaw'], color='blue',label='yaw')
-# ax[0,1].set_xlabel('time')
-# ax[0,1].legend()
-# ax[0,1].grid()
+#subplot of vx vy vz errors
+ax[1,0].set_ylim(-1,1)
+ax[1,0].plot(time2, mynhfclog['e_vx'], color='red', label='x_vel_err')
+ax[1,0].plot(time2, mynhfclog['e_vy'], color='green', label='y_vel_err')
+ax[1,0].plot(time2, mynhfclog['e_vz'], color='blue',label='z_vel_err')
+ax[1,0].set_xlabel('time')
+ax[1,0].legend()
+ax[1,0].grid()
 
-# #subplot of vx vy vz values
-# ax[1,0].set_ylim(-2,2)
-# ax[1,0].plot(time, mylog['vx'], color='red', label='x_vel')
-# ax[1,0].plot(time, mylog['vy'], color='green', label='y_vel')
-# ax[1,0].plot(time, mylog['vz'], color='blue',label='z_vel')
-# ax[1,0].set_xlabel('time')
-# ax[1,0].legend()
-# ax[1,0].grid()
-
-# #subplot of wx wy wz values
-# ax[1,1].set_ylim(-3,3)
-# ax[1,1].plot(time, mylog['wx'], color='red', label='wx')
-# ax[1,1].plot(time, mylog['wy'], color='green', label='wy')
-# ax[1,1].plot(time, mylog['wz'], color='blue',label='wz')
-# ax[1,1].set_xlabel('time')
-# ax[1,1].legend()
-# ax[1,1].grid()
+#subplot of wx wy wz errors
+ax[1,1].set_ylim(-4,4)
+ax[1,1].plot(time2, mynhfclog['e_wx'], color='red', label='wx_err')
+ax[1,1].plot(time2, mynhfclog['e_wy'], color='green', label='wy_err')
+ax[1,1].plot(time2, mynhfclog['e_wz'], color='blue',label='wz_err')
+ax[1,1].set_xlabel('time')
+ax[1,1].legend()
+ax[1,1].grid()
 
 
 
