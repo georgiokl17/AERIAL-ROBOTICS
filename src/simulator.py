aafbd992d21c5e69a4ca197855a792386e17fe96
Georgio Nasr <georgionasr2014@gmail.com>
2026-04-19 15:54:38 +0200

    creation of new python file

---
+++ src/simulator.py
@@ -0,0 +1,59 @@
+#
+import os
+import time
+#this is my comment ~georgio
+# this connects to components running on the same host (localhost)
+# to instead control components running on the remote computer "hostname" use
+# g = genomix.connect('hostname')
+
+# adapt path to your setup
+
+# load components clients
+# --- setup ----------------------------------------------------------------
+#
+# configure components, to be called interactively
+def setup():
+  wtvr=[]
+
+
+
+
+
+
+# Function that make the quadrotor follow the given trajectory
+def move():
+    
+    
+
+# --- start ----------------------------------------------------------------
+#
+# Spin the motors and servo on current position. To be called interactively
+# I have changed the folder to file path to save the log files
+# The below has been changed to save
+def start():
+  
+  move()
+
+
+
+# --- stop -----------------------------------------------------------------
+#
+# Stop motors. To be called interactively
+def stop():
+   
+
+
+## interactively, one can start the simulation with
+# setup()
+# start()
+## and then for instance set a desired position with
+# nhfc.set_position(0, 0, 1, 0)
+## to stop, use
+# stop
+
+
+def simulation():
+  setup()
+  start()
+  stop()
+  
